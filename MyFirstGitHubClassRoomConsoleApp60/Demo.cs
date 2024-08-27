309a6be66edd9bbf48a5741473941a19f5ac7e70
Lawin Blend Bamarne <lab002@edu.zealand.dk>
2024-08-27 13:08:00 +0200

    Added demo class

---
+++ MyFirstGitHubClassRoomConsoleApp60/Demo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstGitHubClassRoomConsoleApp60
+{
+    internal class Demo
+    {
+            public string brand { get; set; }
+            public string model { get; set; }
+            public string year { get; set; }
+            public string corlor { get; set; }
+    }
+}
--- MyFirstGitHubClassRoomConsoleApp60/Program.cs
+++ MyFirstGitHubClassRoomConsoleApp60/Program.cs
@@ -1,7 +1,1 @@
-﻿public class demo
-{
-    public string brand { get; set; }
-    public string model { get; set; }
-    public string year { get; set; }
-    public string corlor { get; set; }
-}
+﻿
